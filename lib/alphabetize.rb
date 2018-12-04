451a4a926e627586e1c07561a47813f3993cbabd
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-12-04 15:48:22 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  alph = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
+  
+  alph.each do |letter|
+    arr.each do |phrase|
+      phrase.each_char do |char|
+      binding.pry
+     
+     
+     
+     
+      end
+    end
+  end
 end
